e05c011f040249471924c75e93b1505f4d113a73
Lisichka <zhu.ivanova@mail.ru>
2022-11-16 11:58:30 -0800

    Add project files.

---
+++ ProtoResearch/Browser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoResearch
+{
+    class Browser : IDisposable
+    {
+        internal const byte MaxTries = 5;
+        private const byte MaxConnections = 10; // Defines maximum number of connections per ServicePoint. Be careful, as it also defines maximum number of sockets in CLOSE_WAIT state
+        private const byte MaxIdleTime = 15; // Defines in seconds, how long socket is allowed to stay in CLOSE_WAIT state after there are no connections to it
+        public CookieContainer CookieContainer = new CookieContainer();
+        private HttpClient HttpClient;
+        internal Browser()
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler
+            {
+                AllowAutoRedirect = true, // This must be false if we want to handle custom redirection schemes such as "steammobile"
+                UseProxy = true,
+                UseCookies = true,
+                CookieContainer = CookieContainer,
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+                MaxConnectionsPerServer = MaxConnections
+            };
+            HttpClient = new HttpClient(httpClientHandler) { Timeout = TimeSpan.FromSeconds(60) };
+            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Sutox/0.1");
+        }
+        public void Dispose() => HttpClient.Dispose();
+        internal Task<HttpResponseMessage> UrlPostToHttp(string requestUri, IReadOnlyCollection<KeyValuePair<string, string>> data = null, string referer = null)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            if (data != null)
+            {
+                try
+                {
+                    request.Content = new FormUrlEncodedContent(data);
+                }
+                catch (Exception e)
+                {
+                    //Logger.Log(e);
+                }
+            }
+            if (!string.IsNullOrEmpty(referer))
+            {
+                request.Headers.Referrer = new Uri(referer);
+            }
+
+            var result = HttpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+
+            return result;
+        }
+        internal Task<string> UrlPostToString(string requestUri, IReadOnlyCollection<KeyValuePair<string, string>> data = null, string referer = null)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            if (data != null)
+            {
+                try
+                {
+                    request.Content = new FormUrlEncodedContent(data);
+                }
+                catch (UriFormatException e)
+                {
+                    //Logger.Log($"{ToString()} {e.StackTrace}", Logger.LogType.Red);
+                }
+            }
+            if (!string.IsNullOrEmpty(referer))
+            {
+                request.Headers.Referrer = new Uri(referer);
+            }
+            return HttpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead).Result.Content.ReadAsStringAsync();
+        }
+        internal Task<HttpResponseMessage> UrlGetToHttp(string request, string referer = null)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    HttpRequestMessage requestMsg = new HttpRequestMessage(HttpMethod.Get, request);
+                    if (!string.IsNullOrEmpty(referer))
+                    {
+                        requestMsg.Headers.Referrer = new Uri(referer);
+                    }
+                    HttpResponseMessage httpRespMsg = HttpClient.SendAsync(requestMsg, HttpCompletionOption.ResponseContentRead).Result;
+                    return httpRespMsg;
+                }
+                catch (Exception e)
+                {
+                    //Logger.Log(e);
+                    return null;
+                }
+            });
+        }
+        internal Task<string> UrlGetToString(string request, string referer = null)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    HttpRequestMessage requestMsg = new HttpRequestMessage(HttpMethod.Get, request);
+                    if (!string.IsNullOrEmpty(referer))
+                    {
+                        requestMsg.Headers.Referrer = new Uri(referer);
+                    }
+                    var httpRespMsg = HttpClient.SendAsync(requestMsg, HttpCompletionOption.ResponseContentRead).Result;
+                    string content = httpRespMsg.Content.ReadAsStringAsync().Result;
+                    return content;
+                }
+                catch (Exception e)
+                {
+                    //Logger.Log(e.StackTrace, Logger.LogType.Red);
+                    return null;
+                }
+            });
+        }
+    }
+}
---
+++ ProtoResearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using Google.Protobuf;
+using System;
+using System.IO;
+using System.Text;
+using ProtoResearch;
+namespace ProtoResearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Test data = new();
+            data.SentIds.Add(18);
+            data.SentIds.Add(21);
+            data.SentIds.Add(3123123999);
+            data.SentIds.Add(12);
+
+
+            using (var fileStream = File.Create("testData.bin"))
+            {
+                data.WriteTo(fileStream);
+            }*/
+
+
+            Browser Browser = new Browser();
+
+            string url = "https://api.steampowered.com/ILoyaltyRewardsService/GetReactions/v1?access_token=85f8ec7d3bfb69b706c69adf2df5c063&input_protobuf_encoded=CAMQtuXD05CAgIgB";
+            HttpResponseMessage response = Browser.UrlGetToHttp(url).Result;
+            Stream stream = response.Content.ReadAsStream();
+            Console.WriteLine(stream.Length);
+            MemoryStream ms = new();
+            stream.CopyTo(ms);
+            byte[] bin = ms.ToArray();
+            
+            AlreadySentAwards data = AlreadySentAwards.Parser.ParseFrom(bin);
+
+            Console.WriteLine();
+                       
+        }
+    }
+}
